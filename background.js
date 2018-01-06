0a47da7d9b5a4d333bc31cb70cdaa260eca4528e
Kevin K. Lee <kevin.kik.lee@gmail.com>
2018-01-06 18:37:22 -0500

    Move the dev files to /src

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request) {
-  chrome.tabs.query({ active: true }, function(tabs) {
-    chrome.tabs.create({
-      url: request.url,
-      active: request.active,
-      index: tabs[0].index + 1
-    })
-  })
-})
